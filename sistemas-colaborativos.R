5eb978c9d6e2b63bea114692955e89c4308b44f9
wparedesgt <wparedesgt@gmail.com>
2018-07-05 16:51:32 -0600

    Terminando  los cambios

--- sistemas-colaborativos.R
+++ sistemas-colaborativos.R
@@ -28,3 +28,32 @@ ibcf_model_details
 
 #el top de las peliculas recomendadas
 
+
+n_recommended <- 6
+ibcf_recc_predicted <- predict(object = ibcf_recc_model, newdata = recc_data_test, n = n_recommended)
+ibcf_recc_predicted
+
+
+ibcf_recc_matrix <- sapply(ibcf_recc_predicted@items, function(x){ colnames(ratings_movies)[x]
+})
+
+View(ibcf_recc_matrix[, 1:4])
+
+
+#basado en los usuarios
+
+
+ubcf_recc_model <- Recommender(data = recc_data_train, method = "UBCF")
+ubcf_model_details <- getModel(ubcf_recc_model)
+ubcf_model_details
+
+n_recommended <- 5
+ubcf_recc_predicted <- predict(object = ubcf_recc_model, newdata = recc_data_test, n = n_recommended)
+ubcf_recc_predicted
+
+ubcf_recc_matrix <- sapply(ubcf_recc_predicted@items, function(x){ colnames(ratings_movies)[x] })
+View(ubcf_recc_matrix[, 1:4])
+
+
+recommender_models <- recommenderRegistry$get_entries(dataType = "realRatingMatrix")
+names(recommender_models)
